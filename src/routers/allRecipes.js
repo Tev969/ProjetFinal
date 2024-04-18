b843320808ee0b0ed033fb8cce0c085803baa147
Tev969 <freezuka14@gmail.com>
2024-04-18 12:46:31 +0200

    WIP

--- src/routers/allRecipes.js
+++ src/routers/allRecipes.js
@@ -5,6 +5,7 @@ allRecipesRouter.get("/allRecipes", async (req, res) => {
     try {
       const recipes = await recipesModel.find();
       res.render("allRecipes/index.html.twig", {
+        user: req.session.user,
         recipes,
       });
     } catch (error) {
